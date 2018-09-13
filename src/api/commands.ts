1c5fa925674ad7bcb88aa4b01f9aa862e4270564
genteelson <genteelson@gmail.com>
2018-09-13 14:07:35 +0900

    action resizing

--- src/api/commands.ts
+++ src/api/commands.ts
@@ -1,3 +1,13 @@
+/**
+ * Commands.js
+ *
+ * Released under LGPL License.
+ * Copyright (c) 1999-2017 Ephox Corp. All rights reserved
+ *
+ * License: http://www.tinymce.com/license
+ * Contributing: http://www.tinymce.com/contributing
+ */
+
 import actions from '../core/actions';
 
 declare var tinymce: any;
@@ -7,7 +17,7 @@ const register = function (editor) {
         cksImageSmallSizing: actions.resizing(editor, 200),
         cksImageMiddleSizing: actions.resizing(editor, 320),
         cksImageLargeSizing: actions.resizing(editor, 400),
-        cksImageOriginalSizing: actions.resizing(editor, -1)
+        cksImageOriginalSizing: actions.resizing(editor, 1600)
     }, function (fn, cmd) {
         editor.addCommand(cmd, fn);
     });
@@ -16,23 +26,3 @@ const register = function (editor) {
 export default {
     register
 };
-/*
-import Tools from 'tinymce/core/api/util/Tools';
-import Actions from '../core/Actions';
-
-const register = function (editor, imageUploadTimerState) {
-    Tools.each({
-        mceImageRotateLeft: Actions.rotate(editor, imageUploadTimerState, -90),
-        mceImageRotateRight: Actions.rotate(editor, imageUploadTimerState, 90),
-        mceImageFlipVertical: Actions.flip(editor, imageUploadTimerState, 'v'),
-        mceImageFlipHorizontal: Actions.flip(editor, imageUploadTimerState, 'h'),
-        mceEditImage: Actions.editImageDialog(editor, imageUploadTimerState)
-    }, function (fn, cmd) {
-        editor.addCommand(cmd, fn);
-    });
-};
-
-export default {
-    register
-};
-*/
--- src/core/actions.ts
+++ src/core/actions.ts
@@ -1,8 +1,33 @@
-const resizing = function (editor, size) {
-    return function () {
-        console.log(size);
-    };
+import * as settings from '../api/settings';
+import imgproxy from '../api/imgproxy';
+import imageSize from './image-size';
+
+const getSelectedImage = function (editor) {
+    return editor.selection.getNode();
 };
+
+const getImgproxySettings = function (editor) {
+    return {
+        url: settings.getImgproxyUrl(editor),
+        key: settings.getImgproxyKey(editor),
+        salt: settings.getImgproxySalt(editor)
+    };
+}
+
+const isUseImgproxy = function (editor, img) {
+    const imgproxySettings = getImgproxySettings(editor);
+
+    if (imgproxySettings.url === undefined || imgproxySettings.key === undefined || imgproxySettings.salt === undefined) {
+        return false;
+    } else if (img.src.indexOf(imgproxySettings.url) !== 0) {
+        return false;
+    }
+    return true;
+}
+
+const getOriginalImageUrlFromImgproxyUrl = function (src: string) {
+    return atob(src.slice(src.lastIndexOf('/') + 1, src.lastIndexOf('.')));
+}
 
 const isEditableImage = function (editor, img) {
     const selectorMatched = editor.dom.is(img, 'img:not([data-mce-object],[data-mce-placeholder])');
@@ -10,7 +35,33 @@ const isEditableImage = function (editor, img) {
     return selectorMatched;
 };
 
+const resizing = function (editor, limitSize) {
+    return function () {
+        let selectedImage = getSelectedImage(editor);
+        if (isUseImgproxy(editor, selectedImage)) {
+            const src = getOriginalImageUrlFromImgproxyUrl(selectedImage.src);
+            selectedImage.src = imgproxy.createImgproxySignatureUrl('fit', limitSize, limitSize, 'ce', 0, src, 'png', getImgproxySettings(editor));
+        } else {
+            const originalSize = imageSize.getNaturalImageSize(selectedImage);
+            const size = {w: originalSize.w, h: originalSize.h};
+            if (limitSize !== 1600) {
+                if (originalSize.w > originalSize.h) {
+                    size.w = limitSize;
+                    size.h = -1;
+                } else if (originalSize.w < originalSize.h) {
+                    size.w = -1;
+                    size.h = limitSize;
+                } else {
+                    size.w = limitSize;
+                    size.h = limitSize;
+                }
+            }
+            imageSize.setImageSize(selectedImage, size);
+        }
+    };
+};
+
 export default {
-    resizing,
-    isEditableImage
+    isEditableImage,
+    resizing
 };
